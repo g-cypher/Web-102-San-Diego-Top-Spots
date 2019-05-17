22d16ff986ad57e5c71303c5a4b2a574ce18b9c5
Garrett <cyph17@gmail.com>
2019-05-17 12:52:20 -0700

    top spots

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
-// write your code here
-
-});
+    $.getJSON("data.json", function(response) {
+      $.each(response, function(i, item) {
+        let longitude = item.location[0];
+        let latitude = item.location[1];
+        var $tr = $("<tr>").append(
+          $("<td>").text(item.name),
+          $("<td>").text(item.description),
+          $("<td>").append(
+            `<a class='btn btn-primary btn-sm' href='https://www.google.com/maps?q=${longitude},${latitude}'>Google Maps</a>`
+          )
+        );
+        $tr.appendTo(".table");
+      });
+    });
+  });
+  
